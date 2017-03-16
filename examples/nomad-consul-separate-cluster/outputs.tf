feb94b44980864d25cfe304b37bbc7ef1a5d1a53
Yevgeniy Brikman <brikis98@gmail.com>
2017-03-16 23:04:18 +0000

    Fix outputs

--- examples/nomad-consul-separate-cluster/outputs.tf
+++ examples/nomad-consul-separate-cluster/outputs.tf
@@ -1,47 +1,71 @@
-output "num_servers" {
-  value = "${module.servers.cluster_size}"
+output "num_nomad_servers" {
+  value = "${module.nomad_servers.cluster_size}"
 }
 
-output "asg_name_servers" {
-  value = "${module.servers.asg_name}"
+output "asg_name_nomad_servers" {
+  value = "${module.nomad_servers.asg_name}"
 }
 
-output "launch_config_name_servers" {
-  value = "${module.servers.launch_config_name}"
+output "launch_config_name_nomad_servers" {
+  value = "${module.nomad_servers.launch_config_name}"
 }
 
-output "iam_role_arn_servers" {
-  value = "${module.servers.iam_role_arn}"
+output "iam_role_arn_nomad_servers" {
+  value = "${module.nomad_servers.iam_role_arn}"
 }
 
-output "iam_role_id_servers" {
-  value = "${module.servers.iam_role_id}"
+output "iam_role_id_nomad_servers" {
+  value = "${module.nomad_servers.iam_role_id}"
 }
 
-output "security_group_id_servers" {
-  value = "${module.servers.security_group_id}"
+output "security_group_id_nomad_servers" {
+  value = "${module.nomad_servers.security_group_id}"
 }
 
-output "num_clients" {
-  value = "${module.clients.cluster_size}"
+output "num_consul_servers" {
+  value = "${module.consul_servers.cluster_size}"
 }
 
-output "asg_name_clients" {
-  value = "${module.clients.asg_name}"
+output "asg_name_consul_servers" {
+  value = "${module.consul_servers.asg_name}"
 }
 
-output "launch_config_name_clients" {
-  value = "${module.clients.launch_config_name}"
+output "launch_config_name_consul_servers" {
+  value = "${module.consul_servers.launch_config_name}"
 }
 
-output "iam_role_arn_clients" {
-  value = "${module.clients.iam_role_arn}"
+output "iam_role_arn_consul_servers" {
+  value = "${module.consul_servers.iam_role_arn}"
 }
 
-output "iam_role_id_clients" {
-  value = "${module.clients.iam_role_id}"
+output "iam_role_id_consul_servers" {
+  value = "${module.consul_servers.iam_role_id}"
 }
 
-output "security_group_id_clients" {
-  value = "${module.clients.security_group_id}"
+output "security_group_id_consul_servers" {
+  value = "${module.consul_servers.security_group_id}"
+}
+
+output "num_nomad_clients" {
+  value = "${module.nomad_clients.cluster_size}"
+}
+
+output "asg_name_nomad_clients" {
+  value = "${module.nomad_clients.asg_name}"
+}
+
+output "launch_config_name_nomad_clients" {
+  value = "${module.nomad_clients.launch_config_name}"
+}
+
+output "iam_role_arn_nomad_clients" {
+  value = "${module.nomad_clients.iam_role_arn}"
+}
+
+output "iam_role_id_nomad_clients" {
+  value = "${module.nomad_clients.iam_role_id}"
+}
+
+output "security_group_id_nomad_clients" {
+  value = "${module.nomad_clients.security_group_id}"
 }
