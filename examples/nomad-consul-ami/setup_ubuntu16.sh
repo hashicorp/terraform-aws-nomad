8e66039ca273ec36d2eb59e0effed71ff01c3389
MatthiasScholz <matthias.scholz@gmail.com>
2017-09-27 22:03:10 +0200

    .BUGFIX packer Ubuntu16: docker installation + missing make

--- examples/nomad-consul-ami/setup_ubuntu16.sh
+++ examples/nomad-consul-ami/setup_ubuntu16.sh
@@ -3,11 +3,21 @@ set -e
 
 SCRIPT=`basename "$0"`
 
-echo "[INFO] [${SCRIPT}] Setup git"
-sudo apt install -y git
+# NOTE: git is already preinstalled
+#echo "[INFO] [${SCRIPT}] Setup git"
+#sudo apt install -y git
 
+# Using Docker CE directly provided by Docker
 echo "[INFO] [${SCRIPT}] Setup docker"
-sudo apt install -y docker.io
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo usermod -a -G docker ec2-user
+cd /tmp/
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+
+sudo apt-get install -y docker-ce
+sudo usermod -a -G docker ubuntu
+
+# Make is needed for the AWS ECR Helper - FIXME: should be removed after setup
+echo "[INFO] [${SCRIPT}] Setup make"
+sudo apt-get install make
