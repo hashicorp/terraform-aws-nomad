d35bbd95082d7d61ab99b79c59ebec2e9058bd1e
Matthias Scholz <matthias.scholz@gmail.com>
2017-11-18 22:11:59 +0100

    .UPDATE ubuntu16 git preinstalled comment

--- examples/nomad-consul-ami/setup_ubuntu16.sh
+++ examples/nomad-consul-ami/setup_ubuntu16.sh
@@ -3,7 +3,7 @@ set -e
 
 SCRIPT=`basename "$0"`
 
-# NOTE: git is already preinstalled
+# NOTE: git is required, but it should already be preinstalled on Ubuntu 16.0
 #echo "[INFO] [${SCRIPT}] Setup git"
 #sudo apt install -y git
 
