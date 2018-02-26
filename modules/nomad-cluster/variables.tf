77b4e57fcc6140b42bfb5bacf6f18b64c022bf9d
Joakim Uddholm <joakim@uddholm.com>
2018-02-26 17:44:26 +0100

    copied cluster_extra_tags functionality from the terraform-aws-vault module.

--- modules/nomad-cluster/variables.tf
+++ modules/nomad-cluster/variables.tf
@@ -171,3 +171,17 @@ variable "security_groups" {
   type = "list"
   default = []
 }
+
+variable "cluster_extra_tags" {
+  description = "A list of additional tags to add to each Instance in the ASG. Each element in the list must be a map with the keys key, value, and propagate_at_launch"
+  type = "list"
+  #example:
+  # default = [
+  #   {
+  #     key = "Environment"
+  #     value = "Dev"
+  #     propagate_at_launch = true
+  #   }
+  # ]
+  default = []
+}
