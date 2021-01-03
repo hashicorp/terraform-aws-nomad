846c3ffa91ca670c6ed62138c3c65800a23e5ee5
Matthias Scholz <matthias.scholz@thoughtworks.com>
2021-01-03 23:58:49 +0100

    :white_check_mark: ssh access test
    Testing SSH access depending on the CIDR block configuration.
    
    Nevertheless due to a bug in the terraform CLI:
    - https://github.com/hashicorp/terraform/issues/17032
    test execution is not possible until the bug is solved.

---
+++ test/nomad_cluster_ssh_test.go
@@ -0,0 +1,13 @@
+package test
+
+import "testing"
+
+func TestNomadClusterSSHAccess(t *testing.T) {
+	t.Parallel()
+	runNomadClusterSSHTest(t, "amazon-linux-2-ami", "ec2-user", true)
+}
+
+func TestNomadClusterSSHNoAccess(t *testing.T) {
+	t.Parallel()
+	runNomadClusterSSHTest(t, "amazon-linux-2-ami", "ec2-user", false)
+}
